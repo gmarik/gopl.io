c40901d743d9a9acc10f5ae4acfde35b7882c17e
gmarik <gmarik@gmail.com>
2015-12-13 17:04:58 -0500

    [Exercises] 02.01 Implemented
    - doesn't do all conversions, only `+`
        k c f
      k - + +
      c ! - +
      f ! + -

--- exercises/ch02/tempconv/tempconv.go
+++ exercises/ch02/tempconv/tempconv.go
@@ -10,6 +10,7 @@ import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	AbsoluteZeroC Celsius = -273.15
@@ -19,11 +20,18 @@ const (
 
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%g°K", k) }
 
 // CToF converts a Celsius temperature to Fahrenheit.
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// KToF converts a Fahrenheit temperature to Kelvin.
+func KToF(k Kelvin) Fahrenheit { return CToF(KToC(k)) }
+
+// CToK converts a Celsius temperature to Kelvin.
+func KToC(k Kelvin) Celsius { return Celsius(k + Kelvin(AbsoluteZeroC)) }
 
 //!-
