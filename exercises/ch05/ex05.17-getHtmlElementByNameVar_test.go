7ee38735fe791ac77e5ed9c61d6219e5c1a83f3e
gmarik <gmarik@gmail.com>
2016-06-12 16:01:38 +0300

    [Exercises] 05.17 Done

---
+++ exercises/ch05/ex05.17-getHtmlElementByNameVar_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+
+	"golang.org/x/net/html"
+)
+
+func ElementsByTagName(n *html.Node, names ...string) []*html.Node {
+	var nodes []*html.Node
+
+	var visit func(*html.Node)
+
+	//depthFirst
+	visit = func(n *html.Node) {
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			if n.Type == html.ElementNode && oneOf(c.Data, names...) {
+				nodes = append(nodes, c)
+			}
+			visit(c)
+		}
+	}
+
+	visit(n)
+
+	return nodes
+}
+
+func oneOf(name string, names ...string) bool {
+	for _, n := range names {
+		if name == n {
+			return true
+		}
+	}
+	return false
+}
+
+func TestElementsByTagName(t *testing.T) {
+
+	doc, err := html.Parse(bytes.NewReader([]byte(htmlSnip)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var got []string
+
+	for _, node := range ElementsByTagName(doc, "head", "a") {
+		got = append(got, node.Data)
+	}
+
+	exp := []string{"head", "a", "a"}
+
+	if !reflect.DeepEqual(got, exp) {
+		t.Errorf("\nExp: %v\nGot: %v", exp, got)
+	}
+}
+
+var htmlSnip = `<html>
+<head>
+</head>
+<body>
+	<img />
+	<a />
+	<a />
+	<br />
+</body>
+</html>
+`
