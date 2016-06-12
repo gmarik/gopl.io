76a35ed5eabbed1879cf1c22ec754b184b38614f
gmarik <gmarik@gmail.com>
2016-06-12 14:51:14 +0300

    [Exercises] 05.16 Done

---
+++ exercises/ch05/ex05.16-variadic-join_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func join(args ...string) string {
+	s := ""
+	for i := 0; i < len(args); i += 1 {
+		s += args[i]
+		if i+1 < len(args) {
+			s += ","
+		}
+	}
+
+	return s
+}
+
+func TestJoin(t *testing.T) {
+
+	cases := []struct {
+		in  []string
+		exp string
+	}{
+		{
+			[]string{},
+			"",
+		},
+		{
+			[]string{"a"},
+			"a",
+		},
+		{
+			[]string{"a", "b"},
+			"a,b",
+		},
+		{
+			[]string{"a", "b", "c"},
+			"a,b,c",
+		},
+	}
+
+	for _, c := range cases {
+		exp := c.exp
+		got := join(c.in...)
+
+		if !reflect.DeepEqual(got, exp) {
+			t.Errorf("\nExp: %v\nGot: %v", exp, got)
+		}
+	}
+}
